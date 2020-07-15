7e52b99f9576524a11ff131f4c1c785fb460a96f
Gabriel Catacora <67804709+catacorag@users.noreply.github.com>
2020-07-14 22:41:06 -0500

    Update phaser_test.js

--- cypress/integration/phaser_test.js
+++ cypress/integration/phaser_test.js
@@ -21,4 +21,49 @@ context("Action", () => {
     cy.get("body").trigger("keydown", { keyCode: 87, release: true });
     cy.wait(6000);
   });
+  it("Cargar mapa", () => {
+    cy.get("#nuevo").click(600, 500);
+    cy.get("#nuevo").click(600, 500);
+    cy.get("body").trigger("keydown", { keyCode: 68, release: true });
+    cy.wait(4000);
+    // cy.get("body").trigger("keyup", { keyCode: 68, release: true });
+    // cy.get("#nuevo").trigger("keydown", { keyCode: 100, release: true });
+    // cy.get("#nuevo").trigger("keydown", { keyCode: 100, release: true });
+    // cy.get("#nuevo").window.
+  });
+  it("Pausar juego", () => {
+    cy.get("#nuevo").click(600, 500);
+    cy.get("#nuevo").click(600, 500);
+    cy.wait(2000);
+    // cy.reload();
+    cy.get("body").trigger("keydown", { keyCode: 70, release: true });
+    cy.wait(1000);
+    cy.get("#nuevo").click(600, 70);
+    cy.get("#nuevo").click(600, 70);
+    cy.get("#nuevo").click(600, 70);
+  });
+  it("Avanzar adelante", () => {
+    cy.get("#nuevo").click(600, 500);
+    cy.get("#nuevo").click(600, 500);
+    cy.wait(5000);
+    // cy.reload();
+    cy.get("body").trigger("keydown", { keyCode: 68, release: true });
+    cy.wait(1000);
+  });
+  it("Saltar", () => {
+    cy.get("#nuevo").click(600, 500);
+    cy.get("#nuevo").click(600, 500);
+    cy.wait(5000);
+    // cy.reload();
+    cy.get("body").trigger("keydown", { keyCode: 87, release: true });
+    cy.wait(1000);
+  });
+  it("Lanzar Poder", () => {
+    cy.get("#nuevo").click(600, 500);
+    cy.get("#nuevo").click(600, 500);
+    cy.wait(5000);
+    // cy.reload();
+    cy.get("body").trigger("keydown", { keyCode: 70, release: true });
+    cy.wait(1000);
+  });
 });
